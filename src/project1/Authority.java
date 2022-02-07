8254a0f8850efc12db40d0ee72725f89cd92ad32
yeonyi <dusdl114@nate.com>
2022-02-07 13:01:31 +0900

    modified

---
+++ src/project1/Authority.java
@@ -0,0 +1,20 @@
+package project1;
+
+public class Authority {
+	private UserRepository ur = UserRepository.getInstance();
+	private User user = null;
+
+	public boolean loginAuthenticate (String id, String password) {
+		if((user = ur.findByKey(id))!=null) {
+			return (user.getId() == id) && (user.getPassword()==password);
+		}//end if
+		return false;
+	}//loginAuthenticate
+
+	public boolean reportAuthenticate (String id) {
+		if((user = ur.findByKey(id))!=null) {
+			return (user.getId().equals("root"));
+		}//end if
+		return false;
+	}//reportAuthenticate 
+}//class
